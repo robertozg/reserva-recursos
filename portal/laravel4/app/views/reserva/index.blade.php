9fa49c6c09c37798fd9118d01efe19824d8d2d15
cris17Porci <cristobal.perez@ceinf.cl>
2013-08-15 23:55:14 -0400

    con informe

--- portal/laravel4/app/views/reserva/index.blade.php
+++ portal/laravel4/app/views/reserva/index.blade.php
@@ -1,6 +1,25 @@
 <!DOCTYPE HTML>
-<html lang="es-ES">
-    <head>
+<html lang="es-ES"><head>   
+    
+    
+    <title>Probando el plugin jQuery de calendario_dw</title>
+	<link href="calendario_dw/calendario_dw-estilos.css" type="text/css" rel="STYLESHEET">
+	<style type="text/css">
+	body{
+		font-family: tahoma, verdana, sans-serif;
+	}
+	</style>
+	<script type="text/javascript" src="calendario_dw/jquery-1.4.4.min.js"></script>
+	<script type="text/javascript" src="calendario_dw/calendario_dw.js"></script>
+	
+	<script type="text/javascript">
+	$(document).ready(function(){
+		$(".campofecha").calendarioDW();
+	})
+	</script>
+	
+          
+    
         <meta charset="UTF-8">
        
        <title>Sistema de Reserva de Recursos - Escuela de Inform&aacute;tica UTEM</title>
@@ -12,48 +31,55 @@
                                 <h3>Pedir un recurso</h3>
              <div class="small-12 columns">
                 <!--pintamos el formulario haciendo uso de la clase form de laravel-->
-            <div class="form">
-              
-                {{ Form::open(array('url' => 'ajax', 'class' => 'register_ajax')) }}
-                
-                {{ Form::label('email', 'Email') }}
- 
-                {{ Form::email('email', Input::old('email')) }}
-                
-                {{ Form::label('username', 'Nick') }}
- 
-                {{ Form::text('username', Input::old('username')) }}
- 
-                {{ Form::label('password', 'Password') }}
- 
-                {{ Form::password('password') }}
- 
-                 {{ Form::label('password_confirmation', 'Confirm password') }}
- 
-                {{ Form::password('password_confirmation') }}
-                
-                <br />
-                {{ Form::submit('Pedir', array("class" => "button expand round")) }}
- 
-                {{ Form::close() }}
+            <div >
+    	                        
+        
+	
+			<form>
+			Fecha: <input type="text" name="fecha" class="campofecha" size="12">
+			</form>   
             </div>
+            
+            
+            
             <!--en este div mostramos el preloader-->
             <div style="margin: 10px 0px 0px 48%" class="before"></div>   
             <!--en este los errores del formulario--> 
             <div class="errors_form"></div>
             <!--en este el mensaje de registro correcto-->
             <div style="display: none" class="success_message alert-box success"></div>     
+            
             </div>       
         </div>
+        
+        <div >
+        <h1> Horario </h1>
+        
+        <?php
+ 
+
+echo"SELECIONE CURSO: <select name='curso'>";
+
+
+
+
+echo "<option value='".$fila['']."'> ".$fila['']."</option>";
+ "</select>";
+
+ 
+?>
+        
+        
+        </div>
+        
         <div class="row">
-            <!--al pulsar en el botón debajo de éste mostraremos los usuarios registrados-->
+            <!--al pulsar en el botï¿½n debajo de ï¿½ste mostraremos los usuarios registrados-->
              <div class="small-12 pull users expand round">    
                 <!--div para mostrar un preloader mientras cargamos los usuarios-->
                 <div style="margin: 10px 0px 0px 48%" class="preload_users"></div>
-                <!--aquí se mostrarán los usuarios-->
+                <!--aquï¿½ se mostrarï¿½n los usuarios-->
                 <div class="load_ajax"></div>
             </div>
         </div>
     </body>
 </html>
-
